bed267278d92da4b8aab80461c7fd04bdd2a46b2
Aryan <au3162233@gmail.com>
2025-11-03 11:43:45 +0530

    titanic dataset added

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-url = "https://raw.githubusercontent.com/scikit-learn/scikit-learn/main/sklearn/datasets/data/iris.csv"
+url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
 df = pd.read_csv(url)
 
-df.to_csv("data/raw/iris.csv", index=False)
+df.to_csv("data/raw/titanic.csv")
