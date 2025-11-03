5c1e9a4e9f3596e946ca851e811c5b27e522bd36
Aryan <au3162233@gmail.com>
2025-11-03 12:05:47 +0530

    Data_preprocessing added

--- src/data/data_preprocessing.py
+++ src/data/data_preprocessing.py
@@ -1,2 +1,9 @@
+import pandas as pd
+import numpy as np
 
+
+df = pd.read_csv("data/raw/titanic.csv")
+df_dropped = df.drop(columns=(["Ticket","Embarked"]),axis=1)
+
+df_dropped.to_csv("data/processed/titanic_preprocessed.csv")
 
